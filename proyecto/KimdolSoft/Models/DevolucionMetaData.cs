c93ea0a6ea25ac04269f9dffd52882e3e5e4f223
santiago28 <santiagovalencia28@outlook.com>
2015-12-07 12:39:15 -0500

    Modificacion devoluciones

---
+++ proyecto/KimdolSoft/Models/DevolucionMetaData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace KimdolSoft.Models
+{
+    public class DevolucionMetaData
+    {
+        [Display(Name = "* ID Proveedor: ")]
+        [Required]
+        public string idProveedor { get; set; }
+
+        [Display(Name = "* Fecha devolución: ")]
+        [Required]
+        public DateTime fechaDevolucion { get; set; }
+
+        [Display(Name = "* Estado: ")]
+        [Required]
+        public string estado { get; set; }
+
+    }
+    [MetadataType(typeof(DevolucionMetaData))]
+    public partial class devolucion
+    {
+
+    }
+}
---
+++ proyecto/KimdolSoft/Models/detalleDevolucionMetaData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace KimdolSoft.Models
+{
+    public class detalleDevolucionMetaData
+    {
+        [Display(Name = "* Producto: ")]
+        [Required]
+        public int idProducto { get; set; }
+
+        [Display(Name = "* Cantidad a devolver: ")]
+        [Required]
+        public int cantidadADevolver { get; set; }
+
+        [Display(Name = "* Cantidad pendiente: ")]
+        [Required]
+        public int cantidadPendiente { get; set; }
+
+        [Display(Name = "* Descripción: ")]
+        [DataType(DataType.MultilineText)]
+        public string descripcion { get; set; }
+    }
+    [MetadataType(typeof(detalleDevolucionMetaData))]
+    public partial class detalledevolucion
+    {
+
+    }
+}
